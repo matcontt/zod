b3f9c26a29d7f5885072845c817cd2bebd8526c9
alejandromateo186 <alejandromateo186@gmail.com>
2025-11-06 01:37:53 +0000

    ✨feat(auth-screens): Creación del esqueleto de pantallas de autenticación

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-2xl font-bold">Login</Text>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import "./global.css"; // Tu archivo CSS global
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)/register" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
